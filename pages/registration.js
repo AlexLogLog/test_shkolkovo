eef9a3c28bfaff3191ba8e9bd979aeecfb4fcf87
Александра Логинова <alexvoy.voy@yandex.ru>
2021-10-05 14:17:42 +0300

    test_1

--- pages/registration.js
+++ pages/registration.js
@@ -10,7 +10,7 @@ export default function Registration() {
         mainApi
             .newUser(user)
             .then((res) => {
-                alert('Вы успешно зарегестрированы.')
+                alert('Вы успешно зарегистрированы.')
             })
             .catch(() => alert('Ошибка входа'))
     }
